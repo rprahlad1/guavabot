cfcbf76f7e981a89a15e7b43ffba24ade43e7068
Samarpita Patra <s.patra@berkeley.edu>
2019-04-26 20:31:23 -0700

    mst, dijkstra's on each bot location. score: 85.29

--- solver.py
+++ solver.py
@@ -8,8 +8,8 @@ def solve(client):
 
     all_students = list(range(1, client.students + 1))
     non_home = list(range(1, client.home)) + list(range(client.home + 1, client.v + 1))
-
     student_response = {} #vertex : bots or no bots
+    bot_locations = []
     for i in non_home:
         responses = client.scout(i, all_students)
         count_T = 0
@@ -22,15 +22,23 @@ def solve(client):
 
         if count_T >= count_F:
             student_response[i] = True
+            bot_locations.append(i)
         else:
             student_response[i] = False
 
+    #MST
+    mst = nx.minimum_spanning_tree(client.G)
+
+    for b in bot_locations:
+        path = nx.dijkstra_path(mst, b, client.home)
+        for i in range(len(path)-1):
+            client.remote(path[i], path[i+1])
+
     #MST lol
-    mst = list(nx.minimum_spanning_edges(client.G))
-
-
-    for _ in range(100):
-        u, v = random.choice(list(client.G.edges()))
-        client.remote(u, v)
+    # mst_edges = list(nx.minimum_spanning_edges(client.G))
+    #
+    # for _ in range(100):
+    #     u, v, w = random.choice(mst_edges)
+    #     client.remote(u, v)
 
     client.end()
