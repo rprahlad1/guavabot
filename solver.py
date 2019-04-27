f020732c53abbdbdb83b3ee8de01aeb1330e209a
Riya Prahlad <riya.prahlad@berkeley.edu>
2019-04-26 20:10:05 -0700

    idk

--- solver.py
+++ solver.py
@@ -8,30 +8,10 @@ def solve(client):
 
     all_students = list(range(1, client.students + 1))
     non_home = list(range(1, client.home)) + list(range(client.home + 1, client.v + 1))
-    student_response = {} #vertex : bots or no bots
-    for i in non_home:
-        responses = client.scout(i, all_students)
-        count_T = 0
-        count_F = 0
-        for r in responses.values():
-            if r == True:
-                count_T += 1
-            else:
-                count_F += 1
-
-        if count_T >= count_F:
-            student_response[i] = True
-        else:
-            student_response[i] = False
-
-    #MST
-    mst = list(nx.minimum_spanning_edges(client.G))
+    client.scout(random.choice(non_home), all_students) 
 
     for _ in range(100):
-
-        u,v,w = random.choice(mst)
-        if u != client.home:
-            if student_response[u] == True:
-                client.remote(u, v)
+        u, v = random.choice(list(client.G.edges()))
+        client.remote(u, v)
 
     client.end()
