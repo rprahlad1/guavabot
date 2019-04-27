770b6b81ecfb3f2a49d300ca856bf4e474851572
Samarpita Patra <s.patra@berkeley.edu>
2019-04-26 21:38:21 -0700

    added bot_maybe to find all the bots incase students lie

--- solver.py
+++ solver.py
@@ -10,6 +10,7 @@ def solve(client):
     non_home = list(range(1, client.home)) + list(range(client.home + 1, client.v + 1))
     student_response = {} #vertex : bots or no bots
     bot_locations = []
+    bot_maybe = []
     for i in non_home:
         responses = client.scout(i, all_students)
         count_T = 0
@@ -20,14 +21,18 @@ def solve(client):
             else:
                 count_F += 1
 
-        if count_T >= count_F:
+        if count_T > count_F:
             student_response[i] = True
             bot_locations.append(i)
         else:
             student_response[i] = False
+            bot_maybe.append(i)
+
 
     #MST
     mst = nx.minimum_spanning_tree(client.G)
+
+    num_bots = 0
 
     #go thru bots that have majority yes and remote all the way home
     for b in bot_locations:
@@ -37,6 +42,25 @@ def solve(client):
             # stops remoting if there were no bots there
             if num == 0:
                 break
+
+            if path[i+1] == client.home:
+                num_bots += num
+
+    if num_bots < client.bots:
+        for b in bot_maybe:
+            path = nx.dijkstra_path(mst, b, client.home)
+            for i in range(len(path)-1):
+                num = client.remote(path[i], path[i+1])
+                    # stops remoting if there were no bots there
+                if num == 0:
+                    break
+                if path[i+1] == client.home:
+                    num_bots += num
+            if num_bots == client.bots:
+                break
+
+
+
 
     #changes to make:
     #try to remote from farthest vertex to reduce repetition
