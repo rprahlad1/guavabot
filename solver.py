c36c1ec1c0075a7b76604fc3391cace6436bb736
Samarpita Patra <s.patra@berkeley.edu>
2019-04-28 16:11:42 -0700

    find all bots first before remoting them home

--- solver.py
+++ solver.py
@@ -2,10 +2,7 @@
 import networkx as nx
 import random
 
-def solve(client):
-    client.end()
-    client.start()
-
+def findbots(client):
     all_students = list(range(1, client.students + 1))
     non_home = list(range(1, client.home)) + list(range(client.home + 1, client.v + 1))
     student_response = {} #vertex : bots or no bots
@@ -28,41 +25,58 @@ def solve(client):
             student_response[i] = False
             bot_maybe.append(i)
 
+    return bot_locations, bot_maybe
+
+
+def solve(client):
+    client.end()
+    client.start()
+
+    bot_locations, bot_maybe = findbots(client)
+
 
     #MST
     mst = nx.minimum_spanning_tree(client.G)
 
     num_bots = 0
-    paths = [] #tuples of (path,length)
-    #go thru bots that have majority yes and remote all the way home
+
+    #tuple: (len(path), path)
+    foundbots= []
+
+    #findbots from bot_locations and bots_maybe
     for b in bot_locations:
         path = nx.dijkstra_path(mst, b, client.home)
-        tup = (path, len(path))
-        paths.append(tup)
+        num = client.remote(path[0], path[1])
+        if num != 0:
+            p = (len(path)-1, path[1:])
+            foundbots.append(p)
+            num_bots += 1
 
-    for path in sorted(paths, key=lambda x: x[1], reverse=True):
-        for i in range(len(path[0])-1):
-            num = client.remote(path[0][i], path[0][i+1])
-            # stops remoting if there were no bots there
-            if num == 0:
-                break
-
-            if path[0][i+1] == client.home:
-                num_bots += num
-
-
-    if num_bots < client.bots:
-        for b in bot_maybe:
+    if num_bots != client.bots:
+        for m in bot_maybe:
             path = nx.dijkstra_path(mst, b, client.home)
-            for i in range(len(path)-1):
-                num = client.remote(path[i], path[i+1])
-                    # stops remoting if there were no bots there
-                if num == 0:
-                    break
-                if path[i+1] == client.home:
-                    num_bots += num
+            num = client.remote(path[0], path[1])
+            if num != 0:
+                p = (len(path)-1, path[1:])
+                foundbots.append(p)
+                num_bots += 1
             if num_bots == client.bots:
                 break
+
+    #get all bots home 
+    bots_home = 0
+    for bot in foundbots:
+        lenbot = bot[0]
+        botpath = bot[1]
+        for i in range(lenbot-1):
+            client.remote(botpath[i], botpath[i+1])
+            if botpath[i+1] == client.home:
+                bots_home += 1
+
+    print(bots_home)
+
+
+
 
 
 
