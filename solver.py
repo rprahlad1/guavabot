c1a25a069ace5335321f7353aa197afdfae97a48
Samarpita Patra <s.patra@berkeley.edu>
2019-04-30 18:48:22 -0700

    merge conflicts

--- solver.py
+++ solver.py
@@ -2,10 +2,9 @@
 import networkx as nx
 import random
 import operator
-import numpy as np
 
-def update_weight(client, s_weight, s_loss):
-    epsilon = np.sqrt(np.log(client.students)/client.v)
+def update_weight(s_weight, s_loss):
+    epsilon = 0.5
     return s_weight*((1-epsilon)**(s_loss))
 
 
@@ -30,51 +29,45 @@ def findbots(client, mst):
         #scout
         student_response[i] = client.scout(i, all_students)
         scores[i] = sum(student_response[i].values())
-        print(scores)
         # bot_locations[i] = 0
 
-    #bots = []    #where students say bots are
-    while bots_found < client.bots: #and scores:
-        if scores:
-            max_vertex = max(scores.items(), key=operator.itemgetter(1))[0]
-        else:
-            break
+    bots = []    #where students say bots are
+    while bots_found < client.bots and scores:
+        max_vertex = max(scores.items(), key=operator.itemgetter(1))[0]
+
         path = nx.dijkstra_path(mst, max_vertex, client.home)
         scores.pop(max_vertex)
 
         #incrementing bots_found HERE
-
         if path[0] not in remoted_to:
             num = client.remote(path[0], path[1])
             if num:
                 paths[max_vertex] = path[1:]
             bots_found += num
             remoted_to.append(path[1])
+
+
+
         #update
+        # if student lied (False)
         responses = student_response[max_vertex]
         for stud in responses.keys():
-            if responses[stud] != num: # if student lied (False)
+            if responses[stud] != num:
             #updated weight of student based on lie
                 losses[stud] += 1
-                new_weight = update_weight(client, student_weights[stud], losses[stud])
-                student_weights[stud] = new_weight if new_weight > 0.5 else 0
-                if losses[stud] >= client.v/2:
-                    student_weights[stud] = 1
-        #normalize student student_weights
-        for s in student_weights.keys():
-            total = sum(student_weights.values())
-            student_weights[s] = student_weights[s]/total if total != 0 else 0
+                student_weights[stud] = update_weight(student_weights[stud], losses[stud])
 
         #update score
         for v in scores.keys():
             resp = student_response[v]
             new_score = 0
             for stud in resp.keys():
-                if resp[stud] == num and num != 0: #if they didn't lie
-                    weight = student_weights[stud]
-                    new_score += weight #*1 if resp[stud] else 0
+                weight = student_weights[stud]
+                new_score += weight*1 if resp[stud] else 0
             scores[v] = new_score
-    #now paths has presumed bot locations : path to home 
+
+
+    #now paths has presumed bot locations : path to home
     return paths
 
 
@@ -86,8 +79,7 @@ def solve(client):
     #MST
     mst = nx.minimum_spanning_tree(client.G)
 
-    #paths = findbots(client, mst)
-    paths = findbots(client, client.graph)
+    paths = findbots(client, mst)
     bots_home = 0
 
     print("REMOTING HOME")
